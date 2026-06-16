ffc2deff1b1f7228756a92148884b476e00aca18
eddy <eddy84113@gmail.com>
2026-06-17 01:46:50 +0800

    feat: move extension UI into side panel

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
-﻿chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
+async function enablePanelOnActionClick() {
+  try {
+    await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+  } catch (error) {
+    console.error('Failed to enable side panel action click behavior.', error);
+  }
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  enablePanelOnActionClick();
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  enablePanelOnActionClick();
 });
